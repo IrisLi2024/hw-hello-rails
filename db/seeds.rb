908739edfa720a94fe559c02921e37fac144d728
Iris <lee1151341952@tamu.edu>
2024-02-18 15:42:31 -0600

    add four movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,19 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+
+    # Add at least three more movies to the seed data 
+  {:title => 'Anatomy of a Fall', :rating => 'R',
+  :release_date => '21-May-2023'},
+  {:title => 'Avatar', :rating => 'PG-13',
+  :release_date => '18-Dem-2009'},
+  {:title => 'Spirited Away', :rating => 'PG',
+  :release_date => '31-Aug-2002'},
+  {:title => '3 Idiots', :rating => 'PG-13',
+  :release_date => '23-Dem-2009'}
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by(movie)
 end
